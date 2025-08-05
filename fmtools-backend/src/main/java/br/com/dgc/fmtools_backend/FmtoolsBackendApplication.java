93c16db75d34541f766d529fc0e33131bdb6d82b
Denison Gonçalves Corbal <denison.corbal@gmail.com>
2025-08-05 17:30:27 -0300

    chore(backend): Base backend project with Spring Boot

---
+++ fmtools-backend/src/main/java/br/com/dgc/fmtools_backend/FmtoolsBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.dgc.fmtools_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmtoolsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmtoolsBackendApplication.class, args);
+	}
+
+}
